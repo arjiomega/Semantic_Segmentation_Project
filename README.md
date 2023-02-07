1217bec2d535643b023e5466675ece7d87ad25be
Richard Omega <richardjoseph.omega@gmail.com>
2023-02-07 08:33:41 +0800

    load and save data

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# Semantic_Segmentation_Project
+# Semantic Segmentation Project
+
+## Virtual Environment Setup
+```bash
+conda create --no-default-packages -n <env_name>
+conda activate <env_name>
+conda install python=3.9
+```
+
+## Load packages
+```bash
+pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from setuptools import find_namespace_packages, setup
+
+# Load packages from requirements.txt
+BASE_DIR = Path(__file__).parent
+
+with open(Path(BASE_DIR,"requirements.txt"),"r") as file:
+    required_packages = [ln.strip() for ln in file.readlines()]
+
+
+setup(
+    name="Semantic Segmentation Project",
+    version=0.1,
+    description=" ",
+    author="Richard Joseph Omega",
+    author_email="richardjoseph.omega@gmail.com",
+    url="https://github.com/arjiomega/Semantic_Segmentation_Project",
+    python_requires=">=3.9",
+    packages=find_namespace_packages(),
+    install_requires=[required_packages],
+)
